d3636cb91c2afe5c4cfbd77803c46167a631538d
jcw1031 <jcw001031@gmail.com>
2024-05-27 11:22:25 +0900

    remove mentee codes

--- src/main/java/com/woopaca/javamentoring/beak/problem2/Main2.java
+++
@@ -1,7 +0,0 @@
-package com.woopaca.javamentoring.beak.problem2;
-
-public class Main2 {
-    public static void main(String[] args) {
-        LibraryManager lm = new LibraryManager();
-    }
-}
--- src/main/java/com/woopaca/javamentoring/choi/problem2/problem2_Main.java
+++
@@ -1,8 +0,0 @@
-package com.woopaca.javamentoring.choi.problem2;
-
-public class problem2_Main {
-	public static void main(String[] args) {
-		problem2_LibraryManager lm = new problem2_LibraryManager();
-	}
-
-}
